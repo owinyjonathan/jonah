7eed70f5dc25678043245b64c024b27436d8e60f
owinyjonathan <144556391+owinyjonathan@users.noreply.github.com>
2025-03-23 17:14:10 +0300

    Add files via upload

---
+++ jonathan.c
@@ -0,0 +1,31 @@
+
+
+#include <stdio.h>
+
+int main(){
+
+    int balance = 65000;      /*accunt balance*/
+    int withdraw = 20000;     /*amount to withdraw*/
+    int new_balance;
+
+    new_balance = balance - withdraw;    /*calculate balance*/
+    
+    printf("balance:%d\n", balance);        /*accunt balance*/     
+    printf("withdraw:%d\n",withdraw);       /*amount to withdraw*/
+    printf("Thank you our dear customer. your New Balance is:%d\n",new_balance);  /*accnt balance*/
+    
+
+    
+    
+    return 0;  
+    }
+
+    
+
+
+
+   
+
+
+
+
